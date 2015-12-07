e37c0811bf9503a964b9fc7bb3bbd029acab6f0d
CandylabS <ssmilkshake@shis-MacBook-Pro.local>
2015-12-06 22:12:01 -0500

    labRead

--- Evaluation/labRead.m
+++ Evaluation/labRead.m
@@ -1,6 +1,6 @@
 % y: n * 2 matrix of boundaries in double
 
-function [y] = labRead(filename);
+function [y] = labRead(filename)
 fid = fopen(filename,'r');
 lines = textscan(fid,'%s','Delimiter','\n','HeaderLines',1);
 lines = lines{1};
