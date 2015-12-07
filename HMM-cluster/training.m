bb49de4a4f18969f1917e72d6041b8e51e86c1ae
Hua Xiao <shawn.huaxiao@gmail.com>
2015-12-06 22:25:31 -0500

    get segment boundaries

--- HMM-cluster/training.m
+++ HMM-cluster/training.m
@@ -8,9 +8,7 @@ groundTruthFileList = dir(groundTruthDir);
 audioFileList       = dir(audioDir);
 numOfFiles          = (length(audioFileList)-4);
 
-% generate a normal distribution for training
-indicesOfNorm = [-1:.1:1];
-norm = normpdf(indicesOfNorm,0,1);
+
 for i = 5:length(audioFileList)
     
     %is current fileID a folder?
@@ -24,36 +22,10 @@ for i = 5:length(audioFileList)
         for j = 1:length(filesListOfSubfolder)
            % For each audio file, do the following:
            
-           % 1. obtain low level feature
-           
 
-           % generate a emission probability matrix for training
-            emsProb  = zeros(80,21); % initialization
-            for k=1:80
-                emsProb(k,:)=norm;
-            end
-           % generate a transition probability matrix for training
-           transProb = ones(80,80)./80;
-           
-           % train the HMM with the initial emission probability matrix and
-           % transmition probability matrix
-           [estTR,estE] = hmmtrain(seqs,transProb,emsProb);
-
-           
-        end
-
-
-
-% train the 80-state HMM by using initial 
-
-% add
            
         end
     end
-  %  [X,fs]    = audioread([audioDir,'/',audioList(i+1).name]);
 
-%preprocessing for input audio file
-
-
-
+           
 end
